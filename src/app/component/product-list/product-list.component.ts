123d8de5c0411486959f7bd8d7049966ac5726b7
Marcio Coelho <marciocoelho@gmail.com>
2021-10-07 22:57:02 -0300

    Created product-list.component.*

---
+++ src/app/component/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+import { products } from "../../products";
+
+@Component({
+	selector:    "app-product-list",
+	templateUrl: "./product-list.component.html"
+})
+export class ProductListComponent {
+	public products = products;
+
+	public share() {
+		window.alert("The product has been shared!");
+	}
+
+	public onNotify() {
+		window.alert("You will be notified when the product goes on sale");
+	}
+}
