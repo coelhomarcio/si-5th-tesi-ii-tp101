f83fb264e99633959a1c7d483ab366f70677dd95
Marcio Coelho <marciocoelho@gmail.com>
2021-10-07 22:53:36 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }                from "@angular/core";
+import { BrowserModule }           from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule }  from "./module/app-routing.module";
+import { AppMaterialModule } from "./module/material.module";
+
+import { AppComponent }           from "./app.component";
+import { TopBarComponent }        from "./component/top-bar/top-bar.component";
+import { BottomBarComponent }     from "./component/bottom-bar/bottom-bar.component";
+import { ProductListComponent }   from "./component/product-list/product-list.component";
+import { ProductAlertsComponent } from "./component/product-alerts/product-alerts.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		TopBarComponent,
+		BottomBarComponent,
+		ProductListComponent,
+		ProductAlertsComponent
+	],
+	imports:      [
+		BrowserModule,
+		BrowserAnimationsModule,
+		AppRoutingModule,
+		AppMaterialModule
+	],
+	providers:    [],
+	bootstrap:    [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
